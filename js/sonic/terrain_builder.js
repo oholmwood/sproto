80fa8ae4e02b52c291e1b5660223f45edc85419f
Ollie Holmwood <ollie@ollie-holmwoods-imac-6.local>
2013-12-15 00:56:41 +0000

    Finally got the principals of tile creation working!

--- js/sonic/terrain_builder.js
+++ js/sonic/terrain_builder.js
@@ -80,12 +80,14 @@ define([
 			self.line_shape.graphics.setStrokeStyle(2).beginStroke("#000000");
 			
 			if(self.points.length > 0)
-			{				
+			{	
+				/*			
 				self.line_shape.graphics.moveTo(self.points[0].x, self.points[0].y);
 				for(var i in self.points)
 				{
 					self.line_shape.graphics.lineTo(self.points[i].x, self.points[i].y);
-				}
+				}*/
+				self.line_shape.graphics.moveTo(self.points[self.points.length-1].x, self.points[self.points.length-1].y);
 				self.line_shape.graphics.lineTo(cursor.x, cursor.y);
 			}
 			
@@ -95,28 +97,85 @@ define([
 		mouse_down:function(e)
 		{
 			var self = this;
-			self.points.push({x:self.last_move.x, y:self.last_move.y});
+			self.points.push({x:Math.round(self.last_move.x), y:Math.round(self.last_move.y)});
 			if(self.points.length > 1)
 			{				
-				self.convert_lines_to_tiles(self.points[self.points.length-1], self.points[self.points.length-2]);
+				self.convert_lines_to_tiles(self.points[self.points.length-2], self.points[self.points.length-1]);
 			}
 		},
 		
 		convert_lines_to_tiles:function(start, end)
 		{
 			var self = this;
-			//var last_tile = self.tiles[self.get_tile(start.x)][self.get_tile(start.y)];
-			self.calculate_line_in_tile(start, maths.get_angle_to_point(start.x, end.x, start.y, end.y)-maths.rads(90));
+			self.calculate_line_in_tile(start, end);
 		},
 		
-		calculate_line_in_tile:function(start, angle)
+		calculate_line_in_tile:function(start, end)
 		{
 			var self = this;
+			
 			var x = start.x % self.tile_size;
 			var y = start.y % self.tile_size;
-			var xa = (self.tile_size - x) / Math.tan(angle);
-			var ya = (self.tile_size - y) / Math.tan(angle);
-			console.log(x +', '+ y + ', ' + maths.degs(angle) + ', ' + xa + ', ' + ya);
+			
+			if(start.x > end.x && x == 0)
+				x = self.tile_size;
+				
+			if(start.y > end.y && y == 0)
+				y = self.tile_size;
+			
+			var x_length_in_square = (self.tile_size - x);
+			if(start.x > end.x)
+			{
+				x_length_in_square = -x;
+				if(x_length_in_square == 0)
+					x_length_in_square = -(self.tile_size);
+			}
+			
+			var y_length_in_square = (self.tile_size - y);
+			if(start.y > end.y)
+			{
+				y_length_in_square = -y;
+				if(y_length_in_square == 0)
+					y_length_in_square = -(self.tile_size);
+			}
+
+			var x_to_y_percent = (end.x - start.x) / (end.y - start.y);
+			var y_to_x_percent = (end.y - start.y) / (end.x - start.x);
+			
+			var x_end = Math.round(y_length_in_square * x_to_y_percent);
+			var y_end = Math.round(x_length_in_square * y_to_x_percent);
+			
+			
+			self.line_shape.graphics.setStrokeStyle(1).beginStroke("#FF3929");
+			self.line_shape.graphics.moveTo(start.x, start.y);
+			self.line_shape.graphics.lineTo(start.x, start.y + y_length_in_square);
+			self.line_shape.graphics.lineTo(start.x + x_end, start.y + y_length_in_square);
+			self.line_shape.graphics.moveTo(start.x, start.y);
+			self.line_shape.graphics.lineTo(start.x + x_length_in_square, start.y);
+			self.line_shape.graphics.lineTo(start.x + x_length_in_square, start.y + y_end);
+			
+			var ret = {sx:x, sy:y, ex:0, ey:0};
+			
+			if(Math.abs(Math.round(y_end)) < Math.abs(Math.round(x_end)))
+			{
+				ret.ey = y + y_end;
+				ret.ex = start.x < end.x ? self.tile_size : 0;
+			}
+			else if(Math.abs(Math.round(y_end)) > Math.abs(Math.round(x_end)))
+			{
+				ret.ex = x + x_end;
+				ret.ey = start.y < end.y ? self.tile_size : 0;
+			}
+			else
+			{
+				ret.ex = x + x_end;
+				ret.ey = y + y_end;
+			}
+				
+			console.log("start: x: " + ret.sx + ", y: " + ret.sy + " end: x: " + ret.ex + " y: " + ret.ey);	
+			
+
+			self.canvas.update();
 		},
 		
 		get_tile:function(val)
